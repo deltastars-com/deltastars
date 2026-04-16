5305df4739dc6fb9ff93aa33715088decbca6665
deltastars <deltastars90@gmail.com>
2026-04-16 05:44:41 +0300

    Update App.tsx
    Signed-off-by: deltastars <deltastars90@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './components/HomePage';
 import { ProductsPage } from './components/ProductsPage';
 import { CartPage } from './components/CartPage';
@@ -9,40 +9,10 @@ import { DeveloperDashboardPage } from './components/DeveloperDashboardPage';
 import { OrderTrackingPage } from './components/OrderTrackingPage';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
-import { useCart } from './hooks/useCart';
-import { AuthProvider, useAuth } from './contexts/AuthContext';
+import { AuthProvider } from './contexts/AuthContext';
 import { DeveloperAuthProvider } from './contexts/DeveloperAuthContext';
 import { ToastProvider } from './contexts/ToastContext';
 import { I18nProvider } from './contexts/I18nContext';
-
-function AppContent() {
-  const { itemCount } = useCart();
-  const { user, logout } = useAuth();
-
-  return (
-    <div className="min-h-screen bg-white flex flex-col">
-      <Header 
-        setPage={() => {}} 
-        cartItemCount={itemCount} 
-        user={user} 
-        onLogout={logout} 
-        onToggleAiAssistant={() => {}} 
-      />
-      <main className="flex-grow pt-32">
-        <Routes>
-          <Route path="/" element={<HomePage setPage={() => {}} setSelectedProductId={() => {}} />} />
-          <Route path="/products" element={<ProductsPage setPage={() => {}} setSelectedProductId={() => {}} />} />
-          <Route path="/cart" element={<CartPage setPage={() => {}} clearCart={() => {}} />} />
-          <Route path="/login" element={<LoginPage setPage={() => {}} />} />
-          <Route path="/dashboard" element={<AdminDashboardPage setPage={() => {}} />} />
-          <Route path="/dev-console" element={<DeveloperDashboardPage onLogout={() => {}} />} />
-          <Route path="/track-order" element={<OrderTrackingPage />} />
-        </Routes>
-      </main>
-      <Footer setPage={() => {}} />
-    </div>
-  );
-}
 
 function App() {
   return (
@@ -50,9 +20,21 @@ function App() {
       <ToastProvider>
         <AuthProvider>
           <DeveloperAuthProvider>
-            <BrowserRouter>
-              <AppContent />
-            </BrowserRouter>
+            <div className="min-h-screen bg-white flex flex-col">
+              <Header />
+              <main className="flex-grow pt-32">
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/products" element={<ProductsPage />} />
+                  <Route path="/cart" element={<CartPage />} />
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/dashboard" element={<AdminDashboardPage />} />
+                  <Route path="/dev-console" element={<DeveloperDashboardPage />} />
+                  <Route path="/track-order" element={<OrderTrackingPage />} />
+                </Routes>
+              </main>
+              <Footer />
+            </div>
           </DeveloperAuthProvider>
         </AuthProvider>
       </ToastProvider>
