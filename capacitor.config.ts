e6565430640d30b88ead84132fd662cc1bc20be4
deltastars <deltastars90@gmail.com>
2026-04-16 05:31:27 +0300

    Create capacitor.config.ts
    Signed-off-by: deltastars <deltastars90@gmail.com>

---
+++ capacitor.config.ts
@@ -0,0 +1,31 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.deltastars.app',
+  appName: 'Delta Stars',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+    cleartext: true
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#1a3a1a",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#FF922B",
+      splashFullScreen: true,
+      splashImmersive: true
+    },
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"]
+    }
+  }
+};
+
+export default config;
