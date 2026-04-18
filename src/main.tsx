da74660c57cadfbe541402bfb7768d0792c893ba
deltastars <deltastars90@gmail.com>
2026-04-18 08:17:01 +0300

    Update main.tsx
    Signed-off-by: deltastars <deltastars90@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,9 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Root element not found');
-
-createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
-);
+)
