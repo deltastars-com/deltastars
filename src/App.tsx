6258401f76bad0b7b83cec24d8ba50a76565092c
deltastars <deltastars90@gmail.com>
2026-04-18 04:46:19 +0300

    Update App.tsx
    import React, { useState, useEffect } from 'react';
    import { BrowserRouter, Routes, Route } from 'react-router-dom';
    import { HomePage } from './components/HomePage';
    import { ProductsPage } from './components/ProductsPage';
    import { CartPage } from './components/CartPage';
    import { LoginPage } from './components/LoginPage';
    import { AdminDashboardPage } from './components/AdminDashboardPage';
    import { DeveloperDashboardPage } from './components/DeveloperDashboardPage';
    import { OrderReceivingDashboard } from './components/Admin/OrderReceivingDashboard';
    import { OrderTrackingPage } from './components/OrderTrackingPage';
    import { Header } from './components/Header';
    import { Footer } from './components/Footer';
    import { AuthProvider } from './contexts/AuthContext';
    import { DeveloperAuthProvider } from './contexts/DeveloperAuthContext';
    import { ToastProvider } from './contexts/ToastContext';
    import { I18nProvider } from './contexts/I18nContext';
    
    function AppContent() {
      return (
        <div className="min-h-screen bg-white flex flex-col">
          <Header />
          <main className="flex-grow pt-32">
            <Routes>
              <Route path="/" element={<HomePage />} />
              <Route path="/products" element={<ProductsPage />} />
              <Route path="/cart" element={<CartPage />} />
              <Route path="/login" element={<LoginPage />} />
              <Route path="/dashboard" element={<AdminDashboardPage />} />
              <Route path="/dev-console" element={<DeveloperDashboardPage onLogout={() => {}} />} />
              <Route path="/order-receiving" element={<OrderReceivingDashboard />} />
              <Route path="/track-order" element={<OrderTrackingPage />} />
            </Routes>
          </main>
          <Footer />
        </div>
      );
    }
    
    function App() {
      return (
        <I18nProvider>
          <ToastProvider>
            <AuthProvider>
              <DeveloperAuthProvider>
                <BrowserRouter>
                  <AppContent />
                </BrowserRouter>
              </DeveloperAuthProvider>
            </AuthProvider>
          </ToastProvider>
        </I18nProvider>
      );
    }
    
    export default App;
    
    Signed-off-by: deltastars <deltastars90@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -62,3 +62,58 @@ function App() {
 }
 
 export default App;
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HomePage } from './components/HomePage';
+import { ProductsPage } from './components/ProductsPage';
+import { CartPage } from './components/CartPage';
+import { LoginPage } from './components/LoginPage';
+import { AdminDashboardPage } from './components/AdminDashboardPage';
+import { DeveloperDashboardPage } from './components/DeveloperDashboardPage';
+import { OrderReceivingDashboard } from './components/Admin/OrderReceivingDashboard';
+import { OrderTrackingPage } from './components/OrderTrackingPage';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { AuthProvider } from './contexts/AuthContext';
+import { DeveloperAuthProvider } from './contexts/DeveloperAuthContext';
+import { ToastProvider } from './contexts/ToastContext';
+import { I18nProvider } from './contexts/I18nContext';
+
+function AppContent() {
+  return (
+    <div className="min-h-screen bg-white flex flex-col">
+      <Header />
+      <main className="flex-grow pt-32">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<AdminDashboardPage />} />
+          <Route path="/dev-console" element={<DeveloperDashboardPage onLogout={() => {}} />} />
+          <Route path="/order-receiving" element={<OrderReceivingDashboard />} />
+          <Route path="/track-order" element={<OrderTrackingPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <I18nProvider>
+      <ToastProvider>
+        <AuthProvider>
+          <DeveloperAuthProvider>
+            <BrowserRouter>
+              <AppContent />
+            </BrowserRouter>
+          </DeveloperAuthProvider>
+        </AuthProvider>
+      </ToastProvider>
+    </I18nProvider>
+  );
+}
+
+export default App;
